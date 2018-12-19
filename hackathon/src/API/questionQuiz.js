0998229975fc6a7cbd2a67054071133cc1fef2b5
Corentin DESOTOCOBET <corentin.dsc@gmail.com>
2018-12-19 17:44:48 +0100

    JSON API quiz, and component checkboxes, quiz

---
+++ hackathon/src/API/questionQuiz.js
@@ -0,0 +1,73 @@
+const questionQuiz = [
+  {
+    id: 1,
+    question: "Que peut-on faire avec une imprimante 3D ?",
+    answers: [
+      {
+        type: { imageUrl: "", explication: "" },
+        content: "Un hérisson"
+      },
+
+      {
+        type: { imageUrl: "", explication: "" },
+        content: "Un porte-clés"
+      },
+      {
+        type: { imageUrl: "", explication: "" },
+        content: "Un pot à crayon"
+      }
+    ]
+  },
+  {
+    id: 2,
+    question: "Quels matériaux peut-on utiliser pour imprimer en 3D ?",
+    answers: [
+      {
+        type: { imageUrl: "", explication: "" },
+        content: "Le plastique"
+      },
+
+      {
+        type: { imageUrl: "", explication: "" },
+        content: "Le métal"
+      },
+      {
+        type: { imageUrl: "", explication: "" },
+        content: "Le chocolat"
+      }
+    ]
+  },
+  {
+    id: 3,
+    question: "Dans quels secteurs d'activités peut-on imprimer en 3D ?",
+    answers: [
+      {
+        type: {
+          imageUrl: "",
+          explication:
+            "En médecine, on peut désormais fabriquer des prothèses au plus près des besoins du patient."
+        },
+        content: "Médecine"
+      },
+
+      {
+        type: {
+          imageUrl: "",
+          explication:
+            "Il est aussi possible de bâtir des maisons, même des immeubles !"
+        },
+        content: "Construction de batiments"
+      },
+      {
+        type: {
+          imageUrl: "",
+          explication:
+            "Il existe des projets de fabrications de véhicules. Comme des motos entièrement réalisées en impression 3D !"
+        },
+        content: "Transports"
+      }
+    ]
+  }
+];
+
+export default questionQuiz;
