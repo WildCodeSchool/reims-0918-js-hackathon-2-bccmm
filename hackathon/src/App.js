9b438e42f9e57911e6f4811096e3c9f1ebae71b7
CharlesC51 <c.crepin@net-c.fr>
2018-12-20 11:22:09 +0100

    add complex button material ui

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -3,12 +3,14 @@ import Checkboxes from "./Components/Checkboxes";
 import "./App.css";
 import questionQuiz from "./API/questionQuiz";
 import Header from "./Header";
+import HomePage from "./HomePage";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <HomePage />
         {questionQuiz.map((question, index) => (
           <Checkboxes {...question} key={index} />
         ))}
---
+++ hackathon/src/HomePage.js
@@ -0,0 +1,162 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import Typography from "@material-ui/core/Typography";
+
+const styles = theme => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    minWidth: 300,
+    width: "100%"
+  },
+  image: {
+    position: "relative",
+    height: 200,
+    [theme.breakpoints.down("xs")]: {
+      width: "100% !important", // Overrides inline-style
+      height: 100
+    },
+    "&:hover, &$focusVisible": {
+      zIndex: 1,
+      "& $imageBackdrop": {
+        opacity: 0.15
+      },
+      "& $imageMarked": {
+        opacity: 0
+      },
+      "& $imageTitle": {
+        border: "4px solid currentColor"
+      }
+    }
+  },
+  focusVisible: {},
+  imageButton: {
+    position: "relative",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: theme.palette.common.white
+  },
+  imageSrc: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: "cover",
+    backgroundPosition: "center 40%"
+  },
+  imageBackdrop: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.4,
+    transition: theme.transitions.create("opacity")
+  },
+  imageTitle: {
+    position: "relative",
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 4}px ${theme
+      .spacing.unit + 6}px`
+  },
+  imageMarked: {
+    height: 3,
+    width: 18,
+    backgroundColor: theme.palette.common.white,
+    position: "absolute",
+    bottom: -2,
+    left: "calc(50% - 9px)",
+    transition: theme.transitions.create("opacity")
+  }
+});
+
+const images = [
+  {
+    url: "https://www.numerama.com/content/uploads/2016/10/3d-impression.jpg",
+    title: "Impression 3D",
+    width: "30%"
+  },
+  {
+    url:
+      "https://www.abf-escaliers.fr/wp-content/uploads/2017/10/decoupe-laser-slide-1600x1000-1024x640.jpg",
+    title: "Découpe laser",
+    width: "30%"
+  },
+  {
+    url:
+      "http://objectif-photographe.fr/wp-content/uploads/2013/11/lightpainting01.jpg",
+    title: "Light painting",
+    width: "30%"
+  },
+  {
+    url:
+      "https://france3-regions.francetvinfo.fr/bourgogne-franche-comte/sites/regions_france3/files/styles/top_big/public/assets/images/2018/04/25/live-escape-game-1155620_1920-3626707.jpg?itok=pT2h3Pta",
+    title: "Escape game",
+    width: "30%"
+  }
+];
+
+const HomePage = props => {
+  const { classes } = props;
+  return (
+    <div>
+      <h2>Vacancesdigitales</h2>
+      <h4>QUAND ?</h4>
+      <p>Pendant la période des vacances scolaires.</p>
+      <h4>QUI ?</h4>
+      <p>Pour les enfants, à partir de 7 ans.</p>
+      <h4>QUOII ?</h4>
+      <p>
+        Atelier créatifs et ludiques, autour des nouvelles technologies et
+        nouveaux outils assistés par ordinateur.
+      </p>
+      <br />
+      <p>A chaque fois un nouveau thème et des surprises !</p>
+
+      <div className={classes.root}>
+        {images.map(image => (
+          <Grid item xs={6}>
+            <ButtonBase
+              focusRipple
+              key={image.title}
+              className={classes.image}
+              focusVisibleClassName={classes.focusVisible}
+              style={{
+                width: image.width
+              }}
+            >
+              <span
+                className={classes.imageSrc}
+                style={{
+                  backgroundImage: `url(${image.url})`
+                }}
+              />
+              <span className={classes.imageBackdrop} />
+              <span className={classes.imageButton}>
+                <Typography
+                  component="span"
+                  variant="subtitle1"
+                  color="inherit"
+                  className={classes.imageTitle}
+                >
+                  {image.title}
+                  <span className={classes.imageMarked} />
+                </Typography>
+              </span>
+            </ButtonBase>
+          </Grid>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default withStyles(styles)(HomePage);
