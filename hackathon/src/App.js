fb17a28ba00e13cb1e7c290661a30d93658788a3
Michaël BOURMEU <michael.bourmeau@outlook.fr>
2018-12-20 17:58:36 +0100

    css

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -2,44 +2,23 @@ import React, { Component } from "react";
 import "./App.css";
 import QuizImp3D from "./Components/QuizImp3D";
 import Header from "./Header";
-<<<<<<< HEAD
-import Calendar from "./Components/Calendar";
-=======
-import HomePage from "./HomePage";
->>>>>>> 2fff36c24fd9437ca92c8da983c587d1199c7592
+import Calendar from "react-calendar";
+// import HomePage from "./HomePage";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      date: new Date(),
-      fake: new Date(2018, 1)
+      date: new Date("2019-02-11")
     };
   }
 
-  onChange = date => {
-    this.setState({ date });
-  };
-
   render() {
-    console.log(typeof this.state.date);
-    console.log(this.state.date);
-    console.log(this.state.fake);
     return (
       <div>
         <Header />
-<<<<<<< HEAD
-        {questionQuiz.map((question, index) => (
-          <Checkboxes {...question} key={index} />
-        ))}
-        <Calendar />
-        {/* <Calendar value={[new Date(2019, 1, 11), new Date(2019, 1, 15)]} /> */}
-        {/* <Calendar value={[new Date(2019, 1, 18), new Date(2019, 1, 22)]} /> */}
-        {/* <Calendar value={[new Date(2019, 3, 8), new Date(2019, 3, 12)]} />
-        <Calendar value={[new Date(2019, 6, 8), new Date(2019, 6, 12)]} /> */}
-=======
+        <Calendar className="class" activeStartDate={this.state.date} />
         <QuizImp3D />
->>>>>>> 2fff36c24fd9437ca92c8da983c587d1199c7592
       </div>
     );
   }
