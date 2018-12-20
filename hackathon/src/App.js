76698ca470857265a0cb26c3ef6dbc23a1406277
Michel <descotesmichel@aol.com>
2018-12-20 15:39:52 +0100

    home page

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
     return (
       <div>
         <Header />
+        <HomePage />
         <QuizLaser />
         <QuizLightPainting />
         <QuizEscapeGames />
