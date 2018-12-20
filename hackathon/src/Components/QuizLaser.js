441f1e399d25a3d9c9e41240205d965e4093c00c
Corentin DESOTOCOBET <corentin.dsc@gmail.com>
2018-12-20 18:47:09 +0100

    nada

--- hackathon/src/Components/QuizLaser.js
+++ hackathon/src/Components/QuizLaser.js
@@ -1,17 +1,14 @@
 import React from "react";
 import Checkboxes from "./Checkboxes";
-import {questionQuizLaser} from "../API/questionQuiz";
+import { questionQuizLaser } from "../API/questionQuiz";
 import { Link } from "react-router-dom";
-const QuizLaser =() =>{
-    return(
-        <div>
-            {questionQuizLaser.map(
-                (question, index)=>(
-                    <Checkboxes {...question}
-                    key={index}/>
-                )
-            )}
-        </div>
-    );
+const QuizLaser = () => {
+  return (
+    <div>
+      {questionQuizLaser.map((question, index) => (
+        <Checkboxes {...question} key={index} />
+      ))}
+    </div>
+  );
 };
 export default QuizLaser;
