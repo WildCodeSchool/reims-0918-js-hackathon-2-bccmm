c503086665d82389bd390f497b1ca42ce92e20cf
Michaël BOURMEU <michael.bourmeau@outlook.fr>
2018-12-20 13:02:50 +0100

    calendar with react-calendar

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
 import Checkboxes from "./Components/Checkboxes";
+import Calendar from "react-calendar";
 import "./App.css";
 import questionQuiz from "./API/questionQuiz";
 import Header from "./Header";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      date: new Date()
+    };
+  }
+
+  onChange = date => {
+    this.setState({ date });
+  };
+
   render() {
     return (
       <div>
@@ -12,6 +24,7 @@ class App extends Component {
         {questionQuiz.map((question, index) => (
           <Checkboxes {...question} key={index} />
         ))}
+        <Calendar onChange={this.onChange} value={this.state.date} />
       </div>
     );
   }
---
+++ hackathon/src/Components/Calendar.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+// import Calendar from "tui-calendar";
+
+const CalendarPage = () => (
+  <Fragment>
+    <div />
+  </Fragment>
+);
+
+export default CalendarPage;
