582f182519c2645ca1e44e0b817c1879a68ecb12
CharlesC51 <c.crepin@net-c.fr>
2018-12-21 05:58:48 +0100

    router calendar done

--- hackathon/src/Components/QuizLaser.js
+++ hackathon/src/Components/QuizLaser.js
@@ -2,10 +2,25 @@ import React from "react";
 import Checkboxes from "./Checkboxes";
 import { questionQuizLaser } from "../API/questionQuiz";
 import Button from "@material-ui/core/Button";
-
+import { withStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+
+const styles = theme => ({
+  buttonback: {
+    margin: theme.spacing.unit,
+    backgroundColor: "#EF914B",
+    color: "white"
+  },
+  buttoncalendar: {
+    margin: theme.spacing.unit,
+    backgroundColor: "#005F82",
+    color: "white"
+  }
+});
 
 const QuizLaser = props => {
+  const { classes } = props;
   return (
     <div
       style={{
@@ -16,10 +31,25 @@ const QuizLaser = props => {
       {questionQuizLaser.map((question, index) => (
         <Checkboxes {...question} key={index} />
       ))}
-      <Link to="/">
-        <Button>Retour à l'accueil</Button>
-      </Link>
+      <Grid container>
+        <Grid item xs={12} md={1} />
+        <Grid item xs={12} md={3}>
+          <Link to="/">
+            <Button variant="contained" className={classes.buttonback}>
+              Retour à l'accueil
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item xs={12} md={6} />
+        <Grid item xs={12} md={1}>
+          <Link to="/calendar">
+            <Button variant="contained" className={classes.buttoncalendar}>
+              Calendrier
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
     </div>
   );
 };
-export default QuizLaser;
+export default withStyles(styles)(QuizLaser);
--- hackathon/src/Components/QuizLightPainting.js
+++ hackathon/src/Components/QuizLightPainting.js
@@ -2,18 +2,50 @@ import React from "react";
 import Checkboxes from "./Checkboxes";
 import { questionQuizLightPainting } from "../API/questionQuiz";
 import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
-const QuizLightPainting = () => {
+import Grid from "@material-ui/core/Grid";
+
+const styles = theme => ({
+  buttonback: {
+    margin: theme.spacing.unit,
+    backgroundColor: "#EF914B",
+    color: "white"
+  },
+  buttoncalendar: {
+    margin: theme.spacing.unit,
+    backgroundColor: "#005F82",
+    color: "white"
+  }
+});
+
+const QuizLightPainting = props => {
+  const { classes } = props;
   return (
     <div>
       <h2>Le Light Painting</h2>
       {questionQuizLightPainting.map((question, index) => (
         <Checkboxes {...question} key={index} />
       ))}
-      <Link to="/">
-        <Button>Retourà l'accueil</Button>
-      </Link>
+      <Grid container>
+        <Grid item xs={12} md={1} />
+        <Grid item xs={12} md={3}>
+          <Link to="/">
+            <Button variant="contained" className={classes.buttonback}>
+              Retourà l'accueil
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item xs={12} md={6} />
+        <Grid item xs={12} md={1}>
+          <Link to="/calendar">
+            <Button variant="contained" className={classes.buttoncalendar}>
+              Calendrier
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
     </div>
   );
 };
-export default QuizLightPainting;
+export default withStyles(styles)(QuizLightPainting);
